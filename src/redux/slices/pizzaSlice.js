9484d69779792005b9371f417a9b7d1bf174c37b
Manuchehr <manuchehrsaidov998@gmail.com>
2023-11-08 15:35:54 +0500

    fix-bug-commit

--- src/redux/slices/pizzaSlice.js
+++ src/redux/slices/pizzaSlice.js
@@ -22,23 +22,24 @@ const pizzaSlice = createSlice({
   initialState,
 
   reducers: {
-    setItmes(state, action) {
+    setItems: (state, action) => {
       state.items = action.payload;
     },
   },
-  extraReducers: {
-    [fetchPizzas.pending]: (state) => {
-      state.status = "loading";
-      state.items = [];
-    },
-    [fetchPizzas.fulfilled]: (state, action) => {
-      state.items = action.payload;
-      state.status = "success";
-    },
-    [fetchPizzas.rejected]: (state) => {
-      state.status = "error";
-      state.items = [];
-    },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchPizzas.pending, (state) => {
+        state.status = "loading";
+        state.items = [];
+      })
+      .addCase(fetchPizzas.fulfilled, (state, action) => {
+        state.items = action.payload;
+        state.status = "success";
+      })
+      .addCase(fetchPizzas.rejected, (state) => {
+        state.status = "error";
+        state.items = [];
+      });
   },
 });
 export const { setItmes } = pizzaSlice.actions;
